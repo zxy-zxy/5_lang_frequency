c5dbaf510d70c3229242309a7b55cb23b401081b
ivan <sinitsinvanya@gmail.com>
2018-04-15 21:39:31 +0400

    Initial commit

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 # Frequency Analysis of Words
+Script for frequency analysis of words.
+## Usage
+Python >= 3.5 required.
+Script accepts a path to file with text to analysis as an argument.
+ 
+Transforms all words in text to lowercase before analysis. 
 
-[TODO. There will be project description]
-
+### Example input
+```bash
+python lang_frequency.py -filepath data.txt
+```
+### Example output:
+```bash
+Order: 0, frequency: 16, word: the
+Order: 1, frequency: 10, word: I
+Order: 2, frequency: 7, word: of
+Order: 3, frequency: 6, word: in
+Order: 4, frequency: 5, word: am
+Order: 5, frequency: 5, word: black
+Order: 6, frequency: 5, word: their
+Order: 7, frequency: 4, word: souls
+Order: 8, frequency: 4, word: my
+Order: 9, frequency: 4, word: to
+```
 # Project Goals
-
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,58 @@
+import sys
+import re
+import argparse
+from collections import Counter
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-filepath",
+        type=str,
+        required=True,
+        help="Please, provide a filepath"
+    )
+    return parser
 
 
 def load_data(filepath):
-    pass
+    with open(filepath) as file:
+        data = file.read()
+        return data
 
 
-def get_most_frequent_words(text):
-    pass
+def split_text_into_words(multi_line_text):
+    words_list = map(
+        str.lower,
+        re.findall(
+            "\w+",
+            multi_line_text
+        )
+    )
+    return words_list
+
+
+def get_most_frequent_words(words_list):
+    counter = Counter(words_list)
+    top_n_words = 10
+    return counter.most_common(top_n_words)
 
 
 if __name__ == '__main__':
-    pass
+    args_parser = create_parser()
+    args = args_parser.parse_args()
+
+    try:
+        multi_line_text = load_data(args.filepath)
+    except FileNotFoundError:
+        sys.exit("Error has occured while reading file")
+
+    words_list = split_text_into_words(multi_line_text)
+    most_frequent_words = get_most_frequent_words(words_list)
+
+    for order, (word, frequency) in enumerate(most_frequent_words):
+        print("Order: {}, frequency: {}, word: {}".format(
+            order,
+            frequency,
+            word)
+        )
